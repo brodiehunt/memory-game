ad65e638b93f40098f6aa1fa454f177d06a98c37
Brodie Hunt <62501585+brodiehunt@users.noreply.github.com>
2023-11-30 20:26:15 +1100

    Finished the playerInfo component, the timing function for the game, and the player displays

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,9 +1,9 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import styled from 'styled-components';
 import Nav from './Nav';
 import Board from './Board';
 import Modal from './Modal';
-import PlayersInfo from './PlayersInfo'
+import PlayerInfo from './PlayerInfo'
 
 const GameContainer = styled.div`
     padding: 1.5rem;
@@ -16,7 +16,12 @@ const GameContainer = styled.div`
     justify-content: space-between;
     align-items: center;
 
-
+    .players-info-container {
+        width: 100%;
+        display: grid;
+        justify-content: center;
+        grid-template-columns: ${({$numPlayers}) => `repeat(${$numPlayers}, minmax(50px, 250px))`};        grid-gap: 1.5rem;
+    }
 
     @media (min-width: 768px) {
         padding: 2.5rem;
@@ -27,11 +32,63 @@ const GameContainer = styled.div`
     }
 `;
 
-export default function Game({handleNewGame}) {
+export default function Game({numOfPlayers, handleNewGame}) {
+    const [players, setPlayers] = useState([]);
+    const [timeLapsed, setTimeLapsed] = useState(0);
+    // console.log(timeLapsed)
+    useEffect(() => {
+        const initializePlayers = (numPlayers) => {
+            return Array.from({length: numPlayers}, (_, index) => ({
+                name: `Player ${index + 1}`,
+                pairs: 0,
+                turn: index === 0,
+                id: index
+            }));
+        }
 
+        const playersArray = initializePlayers(numOfPlayers);
+        setPlayers(playersArray);
+    }, [numOfPlayers]);
+
+    useEffect(() => {
+        console.log('use effect');
+        const interval = setInterval(() => {
+            console.log('time, ');
+            setTimeLapsed(prevTime => prevTime + 1);
+        }, 1000);
+
+        return () => clearTimeout(interval);
+    }, [])
+
+    function formatTime(time) {
+        const mins = Math.floor(time / 60);
+        let secs = time % 60;
+        if (secs < 10) {
+            secs = '0' + secs;
+        }
+        return `${mins}: ${secs}`;
+    }
+
+    const time = formatTime(timeLapsed);
+
+    const playersInfoDivs = players.map((item) => {
+        return (
+            <PlayerInfo singlePlayer={false} name={item.name} id={item.id} pairs={item.pairs} turn={item.turn} />
+        )
+    })
+    
     return (
-        <GameContainer>
+        <GameContainer $numPlayers={numOfPlayers > 1 ? numOfPlayers : 2}>
             <Nav handleNewGame={handleNewGame}/>
+            <div className="players-info-container">
+                {numOfPlayers > 1 && playersInfoDivs}
+                {numOfPlayers === 1 && 
+                    <>
+                        <PlayerInfo singlePlayer={true} title="Time" val={time} />
+                        <PlayerInfo singlePlayer={true} title="Moves" val="23" />
+                    </>
+                }
+            </div>
         </GameContainer>
     )
 }
--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,6 +1,82 @@
+import styled from 'styled-components';
 
-export default function PlayerInfo() {
+const PlayerDiv = styled.div`
+    padding: 0.6rem;
+    border-radius: 0.3125rem;
+    background-color: ${({$turn}) => $turn ? 'var(--primary-col)' : 'var(--idle-col)'};
+    color: ${({$turn}) => $turn ? 'var(--text-lightest)' : 'var(--text-dark)'};
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    position: relative;
+
+
+    &::before {
+    content: '';
+        display: ${({ $turn }) => ($turn ? 'block' : 'none')};
+        width: 0;
+        height: 0;
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        border-bottom: 10px solid ${({ $turn }) => ($turn ? 'var(--primary-col)' : 'transparent')};
+        position: absolute;
+        top: -10px; 
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    .name {
+        text-align: center;
+        font-size: var(--font-sz-300);
+    }
+    .lrg-name {
+        display: none;
+    }
+    .pairs {
+        font-size: var(--font-sz-700);
+    }
+
+    @media (min-width: 768px) {
+        align-items: start;
+
+        .name {
+            font-size: var(--font-sz-500);
+        }
+        .sml-name {
+            display: none;
+        }
+        .lrg-name {
+            display: block
+        }
+        .pairs {
+            font-size: var(--font-sz-900);
+        }
+
+    }
+
+    @media (min-width: 1444px) {
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+`
+export default function PlayerInfo({name, id, pairs, turn, singlePlayer, title, val}) {
     return (
-        <div>Player stuff</div>
+        singlePlayer ? (
+            <PlayerDiv $turn={false}>
+                <div className="name">{title}</div>
+                <div className="pairs">{val}</div>
+            </PlayerDiv>
+        ) : (
+            <PlayerDiv $turn={turn} key={id}>
+                <div className="sml-name name">
+                    {name[0] + name[name.length -1]}
+                </div>
+                <div className="lrg-name name">
+                    {name}
+                </div>
+                <div className="pairs">{pairs}</div>
+            </PlayerDiv>
+        )
+    
     )
 }
